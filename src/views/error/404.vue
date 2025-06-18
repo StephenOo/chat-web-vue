9e5a091bbec7253444d650089bdd573f29f5beb3
dawei <187533154+dawei1898@users.noreply.github.com>
2025-06-18 17:40:14 +0800

    feat: 404 page

--- src/views/error/404.vue
+++ src/views/error/404.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
-
+import { Button } from '@/components/ui/button'
 </script>
 
 <template>
-<div class="text-center">404</div>
+  <div class="min-h-screen flex flex-col justify-center items-center gap-3">
+    <p class="text-2xl">抱歉！您访问的页面不存在。</p>
+    <p>404 | not fund</p>
+    <RouterLink to="/">
+      <Button class="cursor-pointer"> 返回主页</Button>
+    </RouterLink>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
